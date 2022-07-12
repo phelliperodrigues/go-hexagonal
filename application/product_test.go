f356170b53b51e7109c827f2afe529f28747883e
Phellipe Rodrigues <phelliperodrigues@pop-os.localdomain>
2022-07-11 21:00:38 -0300

    Implement test by enabled product

--- application/product_test.go
+++ application/product_test.go
@@ -1,1 +1,22 @@
-package application
+package application_test
+
+import (
+	"github.com/phelliperodrigues/go-hexagonal/application"
+	"github.com/stretchr/testify/require"
+	"testing"
+)
+
+func TestProduct_Enable(t *testing.T) {
+	product := application.Product{}
+	product.Name = "Hellp"
+	product.Status = application.DISABLED
+	product.Prince = 10
+
+	err := product.Enable()
+	require.Nil(t, err)
+
+	product.Prince = 0
+
+	err = product.Enable()
+	require.Equal(t, "the price must be greater than zero to enable the product", err.Error())
+}
