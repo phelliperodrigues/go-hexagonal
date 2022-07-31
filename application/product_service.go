a1d820011e9089c8602946ca1804e0e5d6aa6c9b
Phellipe Rodrigues <phelliperodrigues.dev@gmail.com>
2022-07-31 15:13:45 -0300

    feat: Implement all methods Product Service and test

--- application/product_service.go
+++ application/product_service.go
@@ -12,6 +12,53 @@ func (s *ProductService) Get(id string) (ProductInterface, error) {
 	return product, nil
 }
 
-//func (s *ProductService) Create(name string, price float64) (ProductInterface, error)
-//func (s *ProductService) Enable(product ProductInterface) (ProductInterface, error)
-//func (s *ProductService) Disable(product ProductInterface) (ProductInterface, error)
+func (s *ProductService) Create(name string, price float64) (ProductInterface, error) {
+	product := NewProduct()
+	product.Name = name
+	product.Prince = price
+
+	_, err := product.IsValid()
+	if err != nil {
+		return &Product{}, err
+	}
+
+	result, err := s.Percistence.Save(product)
+
+	if err != nil {
+		return &Product{}, err
+	}
+
+	return result, nil
+}
+
+func (s *ProductService) Enable(product ProductInterface) (ProductInterface, error) {
+	err := product.Enable()
+
+	if err != nil {
+		return &Product{}, err
+	}
+
+	result, err := s.Percistence.Save(product)
+
+	if err != nil {
+		return &Product{}, err
+	}
+
+	return result, nil
+}
+
+func (s *ProductService) Disable(product ProductInterface) (ProductInterface, error) {
+	err := product.Disable()
+
+	if err != nil {
+		return &Product{}, err
+	}
+
+	result, err := s.Percistence.Save(product)
+
+	if err != nil {
+		return &Product{}, err
+	}
+
+	return result, nil
+}
